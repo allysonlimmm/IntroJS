07b68e8ade2df09443f959662287b383b5ae8c6c
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2021-08-03 17:19:04 +0800

    Update getComic.js

--- getComic.js
+++ getComic.js
@@ -1,104 +1,100 @@
-const internal = require("assert");
+const MAX_COMIC = 2497;
 
 // GETTING COMIC //
 async function getComic(comicNum) {
-    let dataList = [];
-    for (let i = comicNum-1; i <= comicNum+1; i++) {
-        const data = await fetch(`https://intro-to-js-playground.vercel.app/api/xkcd-comics/${i}`);
-        dataList.push(await data.json());
-    }
-    return dataList;
+	const data = await Promise.all(getComicNumbers(comicNum)
+					.map((num) => fetch(`https://intro-to-js-playground.vercel.app/api/xkcd-comics/${num}`)));
+    return await Promise.all(data.map((d) => d.json()));
 }
 
 function getComicTitle(item) {
-    const comicTitle = item['title'];
-    return comicTitle
+    return item['title'];
 }
 
 function getComicImg(item) {
-    const comicImg = item['img'];
-    return comicImg
+    return item['img'];
 }
 
 function getComicAlt(item) {
-    const comicAlt = item['alt'];
-    return comicAlt
+    return item['alt'];
 }
 
-// function validation() {
-//     let x = Number(document.getElementById("comicNum").value);
-//     if (x <= 2496 || x >= 1) {
-//         return x;
-//     }
-//     else {
-//         alert("Invalid number")
-//     }
-// }
+function getComicNumber(comicNum) {
+	const num = comicNum % MAX_COMIC;
+	return num ? num : MAX_COMIC;
+}
 
-async function updateComics(btnID, comicNum) {
+function getComicNumbers(comicNum) {
+	return Array.from({ length: size }, (val, i) => getComicNumber(comicNum - Math.floor(size / 2) + i));
+}
 
+async function updateComics(comicNum) {
     const comicList = document.querySelector('#comicList');
     const comicTitle = document.querySelector('#comicTitle');
-    const btn = document.querySelector(`#${btnID}`);
     const loading = document.querySelector("#loadingDiv");
-    const comicNumSelector = document.querySelector("#comicNum");
-    // const submit = document.querySelector('#submit');
-    
-    // submit.addEventListener('click', function onButtonClick() {
-    //     comicNum = validation()
-    // })    
-
-    btn.addEventListener('click', function onButtonClick() {
-        if (btnID==="prevBtn") {
-            if (comicNum >= 5) {
-                comicNum = comicNum - 3;
-            }
-            else {
-                comicNum = 2;
-                alert("You have reached the start of the comic list.")
-            }
-        }
-        else if (btnID==="randBtn") {
-            comicNum = Math.floor(Math.random() * 2496) + 1;
-        }
-        else if (btnID==="nextBtn") {
-            if (comicNum <= 2492) {
-                comicNum = comicNum + 3
-            }
-            else {
-                comicNum = 2654;
-                alert("You have reached the end of the comic list.")
-            }
-            
-        }
-        updateComics('prevBtn', comicNum);
-        updateComics('randBtn', comicNum);
-        updateComics('nextBtn', comicNum);
-    })
-
-    
-    loading.classList.remove("hidden")
+    const comicNumSelector = document.querySelector("#comicNum");   
+	
     comicList.innerHTML = '';
-    await getComic(comicNum)
-        .then((data) => {
-            loading.classList.add("hidden");
-            comicNumSelector.innerText = `Showing comics ${comicNum-1}/${comicNum}/${comicNum+1} out of 2497`;
-            comicList.innerHTML = '';
-            data.forEach((comic) => {
-                const item = document.createElement("div");
-                item.className = "column";
-                item.innerHTML = `
-                <img src="${getComicImg(comic)}" alt="${getComicAlt(comic)}">
-                `;
-                comicList.appendChild(item);
-            });
-            comicTitle.innerText = getComicTitle(data[1]);
-        });
-    
-
+    loading.classList.remove("hidden")
+    const data = await getComic(comicNum);
+	loading.classList.add("hidden");
+    comicNumSelector.innerText = `Showing comics ${getComicNumbers(comicNum).join("/")} out of ${MAX_COMIC}`;
+	comicList.innerHTML = '';
+	data.forEach((comic) => {
+		const item = document.createElement("div");
+		item.className = "column";
+		item.innerHTML = `
+		<img src="${getComicImg(comic)}" alt="${getComicAlt(comic)}"/>
+		`;
+		comicList.appendChild(item);
+	});
+	comicTitle.innerText = getComicTitle(data[Math.floor(size / 2)]);
 }
 
 let comicNum = 2;
-updateComics('prevBtn', comicNum);
-updateComics('randBtn', comicNum);
-updateComics('nextBtn', comicNum);
+let size = 3;
+
+updateComics(comicNum);
+
+const prevButton = document.getElementById('prevBtn');
+
+prevButton.onclick = function () {
+	comicNum -= size;
+	if (comicNum <= 0) comicNum += MAX_COMIC;
+	updateComics(comicNum);
+};
+
+const nextButton = document.getElementById('nextBtn');
+
+nextButton.onclick = function () {
+	comicNum += size;
+	if (comicNum > MAX_COMIC) comicNum -= MAX_COMIC;
+	updateComics(comicNum);
+};
+
+const randButton = document.getElementById('randBtn');
+
+randButton.onclick = function () {
+	comicNum = Math.floor(Math.random() * 2496) + 1;
+	updateComics(comicNum);
+};
+
+const submitButton = document.getElementById('submit');
+
+submitButton.onclick = function () {
+	const searchInput = document.getElementById('comicInput');
+	const comicInput = Number(searchInput.value);
+	if (isNaN(comicInput) || comicInput < 1 || comicInput > MAX_COMIC) {
+		window.alert('Invalid Search Input');
+	} else {
+		comicNum = comicInput;
+		updateComics(comicNum);
+	}
+};
+
+const sizeSelector = document.getElementById('size');
+
+sizeSelector.onchange = function () {
+	size = Number(sizeSelector.value);
+	updateComics(comicNum);
+}
