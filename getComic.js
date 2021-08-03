3852e161d75f386db8de1ed6f30adb75f49fe2ef
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2021-08-03 14:57:15 +0800

    Update getComic.js

--- getComic.js
+++ getComic.js
@@ -24,7 +24,7 @@ function getComicAlt(item) {
 }
 
 
-async function updateComics(btnID, comicNum) {
+async function updateComics(btnID) {
     let comicNum = 2;
     const comicList = document.querySelector('#comicList');
     const comicTitle = document.querySelector('#comicTitle');
@@ -60,9 +60,9 @@ async function updateComics(btnID, comicNum) {
             }
             
         }
-        updateComics('prevBtn', comicNum);
-        updateComics('randBtn', comicNum);
-        updateComics('nextBtn', comicNum);
+        updateComics('prevBtn');
+        updateComics('randBtn');
+        updateComics('nextBtn');
     })
 
     
