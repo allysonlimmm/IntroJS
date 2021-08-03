507e9250850785596e650ce6f54cb7b51da1b98f
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2021-08-03 18:38:41 +0800

    Update getComic.js

--- getComic.js
+++ getComic.js
@@ -1,33 +1,39 @@
 const MAX_COMIC = 2475;
 
-// GETTING COMIC //
+// get all the relevant comics based on comic number arrays from API
 async function getComic(comicNum) {
-	const data = await Promise.all(getComicNumbers(comicNum)
+	const data = await Promise.all(getComicNumbers(comicNum) 
 					.map((num) => fetch(`https://intro-to-js-playground.vercel.app/api/xkcd-comics/${num}`)));
     return await Promise.all(data.map((d) => d.json()));
 }
 
+// get comic title
 function getComicTitle(item) {
     return item['title'];
 }
 
+// get comic image
 function getComicImg(item) {
     return item['img'];
 }
 
+// get comic alt text
 function getComicAlt(item) {
     return item['alt'];
 }
 
+// handling overflow of comic numbers
 function getComicNumber(comicNum) {
 	const num = comicNum % MAX_COMIC;
-	return num ? num : MAX_COMIC;
+	return num ? num : MAX_COMIC;           // if num is not 0 then show num, if not show MAX_COMIC
 }
 
+// returns an array of length based on number of comics to show, getting the corresponding comic numbers to be shown
 function getComicNumbers(comicNum) {
 	return Array.from({ length: size }, (val, i) => getComicNumber(comicNum - Math.floor(size / 2) + i));
 }
 
+// update webpage to show comics
 async function updateComics(comicNum) {
     const comicList = document.querySelector('#comicList');
     const comicTitle = document.querySelector('#comicTitle');
@@ -39,7 +45,7 @@ async function updateComics(comicNum) {
     const data = await getComic(comicNum);
 	loading.classList.add("hidden");
     comicNumSelector.innerText = `Showing comics ${getComicNumbers(comicNum).join("/")} out of ${MAX_COMIC}`;
-	comicList.innerHTML = '';
+
 	data.forEach((comic) => {
 		const item = document.createElement("div");
 		item.className = "column";
@@ -48,37 +54,42 @@ async function updateComics(comicNum) {
 		`;
 		comicList.appendChild(item);
 	});
-	comicTitle.innerText = getComicTitle(data[Math.floor(size / 2)]);
+	comicTitle.innerText = getComicTitle(data[Math.floor(size / 2)]);       // show comic title of the middle comic
 }
 
+// initialising variables
 let comicNum = 2;
 let size = 3;
 
 updateComics(comicNum);
 
+// identifying prev button and writing function to determine what happens when the button is clicked
 const prevButton = document.getElementById('prevBtn');
-
+// when button clicked, comic num reduces by num of comic showed >> if comicNum <= 0, then it goes back to the last comic
 prevButton.onclick = function () {
 	comicNum -= size;
 	if (comicNum <= 0) comicNum += MAX_COMIC;
 	updateComics(comicNum);
 };
 
+// identifying next button and writing function to determine what happens when the button is clicked
 const nextButton = document.getElementById('nextBtn');
-
+// when button clicked, comic num increases by num of comic showed >> if comicNum exceeds max number of comics, then it goes back to the start
 nextButton.onclick = function () {
 	comicNum += size;
 	if (comicNum > MAX_COMIC) comicNum -= MAX_COMIC;
 	updateComics(comicNum);
 };
 
+// identifying random button and writing function to determine what happens when the button is clicked
 const randButton = document.getElementById('randBtn');
 
 randButton.onclick = function () {
-	comicNum = Math.floor(Math.random() * 2496) + 1;
+	comicNum = Math.floor(Math.random() * 2474) + 1;
 	updateComics(comicNum);
 };
 
+// identifying submit button and updating the comic to show what the user search for
 const submitButton = document.getElementById('submit');
 
 submitButton.onclick = function () {
@@ -92,6 +103,7 @@ submitButton.onclick = function () {
 	}
 };
 
+// identifying size button and updating the number of comics shown
 const sizeSelector = document.getElementById('size');
 
 sizeSelector.onchange = function () {
