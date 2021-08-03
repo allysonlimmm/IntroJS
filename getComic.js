40d98fef71475a7d92d1a99065e89d072d310465
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2021-08-03 14:59:26 +0800

    Update getComic.js

--- getComic.js
+++ getComic.js
@@ -34,7 +34,7 @@ async function updateComics(btnID) {
     const submit = document.querySelector('#submit');
     
     submit.addEventListener('click', function onButtonClick() {
-        comicNum = document.getElementById("comicNum").value;
+        comicNum = document.getElementById("submit").value;
     })    
 
     btn.addEventListener('click', function onButtonClick() {
